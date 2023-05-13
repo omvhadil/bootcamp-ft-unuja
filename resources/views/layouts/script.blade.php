26996591a293b30ced204199078eb966eb8bc4ee
omvhadil <fadil.rohman.327@gmail.com>
2023-05-13 09:16:40 +0700

    latihan laravel satu

---
+++ resources/views/layouts/script.blade.php
@@ -0,0 +1,5 @@
+<script>
+    var hostUrl = "assets/";
+</script>
+<script src="{{ url('assets/metronic/js/plugins.bundle.js') }}"></script>
+<script src="{{ url('assets/metronic/js/scripts.bundle.js') }}"></script>
---
+++ resources/views/layouts/style.blade.php
@@ -0,0 +1,5 @@
+<link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
+<link rel="shortcut icon" href="assets/media/logos/favicon.ico" />
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
+<link href="{{ url('assets/metronic/css/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ url('assets/metronic/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
